2dce59b81ff50a246ff3b583677f1dfcf1d49237
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-09-09 23:56:46 +0300

    Hide the navigation bar on the record screen

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -23,6 +23,18 @@ class RecordSoundsViewController: UIViewController {
         
         stopRecordingButton.isEnabled = false
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
         configureUI(isRecording: true)
